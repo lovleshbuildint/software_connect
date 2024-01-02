bc16297ba34c6b6f6b52cc4bd8dccb16c2859c0c
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-02 11:40:22 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/download_file.dart
+++ lib/custom_code/actions/download_file.dart
@@ -16,14 +16,13 @@ import 'package:permission_handler/permission_handler.dart';
 
 Future<String?> downloadFile(String? url, String? extention, String? deviceId,
     String? path, String? bankName, String? modelName) async {
+  await Permission.manageExternalStorage.request();
+  // Get the directory where the file will be saved
+  var status = await Permission.storage.status;
+  if (status.isDenied) {
+    await Permission.storage.request();
+  }
   try {
-    await Permission.manageExternalStorage.request();
-    // Get the directory where the file will be saved
-    var status = await Permission.storage.status;
-    if (status.isDenied) {
-      await Permission.storage.request();
-    }
-    Directory? directory = await getExternalStorageDirectory();
     // Generate a unique filename using a timestamp
     String directoryPath = 'storage/$deviceId';
     String imagesFolderPath = '$directoryPath/$modelName/$bankName';
@@ -80,6 +79,64 @@ Future<String?> downloadFile(String? url, String? extention, String? deviceId,
     print(downloadstatus);
     return 'File Downloaded';
   } catch (e) {
-    return '$e';
+    try {
+      // Generate a unique filename using a timestamp
+      String directoryPath = '/mnt/media_rw/$deviceId';
+      String imagesFolderPath = '$directoryPath/$modelName/$bankName';
+      String filePath = '$imagesFolderPath/$extention';
+
+// Create the directory if it doesn't exist
+      Directory(imagesFolderPath).createSync(recursive: true);
+
+      // Create an HttpClient instance
+      final client = http.Client();
+
+      // Send the request
+      final http.Request request = http.Request('GET', Uri.parse(url!));
+      final http.StreamedResponse streamedResponse = await client.send(request);
+
+      // Get the total size of the file
+      int totalBytes = streamedResponse.contentLength ?? 0;
+
+      // Keep track of the downloaded bytes
+      int downloadedBytes = 0;
+
+      // Write the downloaded file to the local storage
+      File file = File(filePath);
+      IOSink sink = file.openWrite();
+      String downloadstatus = "";
+      // Listen for updates during the download
+      streamedResponse.stream.listen(
+        (List<int> chunk) {
+          // Write the chunk to the file
+          sink.add(chunk);
+          // Update the downloaded bytes
+          downloadedBytes += chunk.length;
+          // Calculate and print the percentage
+          double percentage = (downloadedBytes / totalBytes) * 100;
+          // print('Downloading: ${percentage.toStringAsFixed(2)}%');
+        },
+        onDone: () {
+          // Close the file when the download is complete
+          sink.close();
+          client.close(); // Close the HttpClient
+          // Show a message to the user that the file has been downloaded
+          print('File downloaded successfully!');
+          downloadstatus = 'File downloaded successfully!';
+        },
+        onError: (error) {
+          // Handle errors during the download
+          print('Error downloading file: $error');
+          sink.close();
+          client.close(); // Close the HttpClient
+          file.delete(); // Delete the file in case of an error
+        },
+        cancelOnError: true,
+      );
+      print(downloadstatus);
+      return 'File Downloaded';
+    } catch (e) {
+      return '$e';
+    }
   }
 }
