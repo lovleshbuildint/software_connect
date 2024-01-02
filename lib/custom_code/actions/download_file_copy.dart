e52b8b4c1fcdadcbbf778d5fb3417981f9552780
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-02 08:10:50 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/download_file_copy.dart
+++ lib/custom_code/actions/download_file_copy.dart
@@ -15,19 +15,23 @@ import 'package:http/http.dart' as http;
 import 'package:permission_handler/permission_handler.dart';
 
 Future<String?> downloadFileCopy(String? url) async {
-  await Permission.manageExternalStorage.request();
-  // Get the directory where the file will be saved
-  var status = await Permission.storage.status;
-  if (status.isDenied) {
-    await Permission.storage.request();
-  }
-  Directory? directory = await getExternalStorageDirectory();
-  // Generate a unique filename using a timestamp
-  String directoryPath = '$url';
+  try {
+    await Permission.manageExternalStorage.request();
+    // Get the directory where the file will be saved
+    var status = await Permission.storage.status;
+    if (status.isDenied) {
+      await Permission.storage.request();
+    }
+    Directory? directory = await getExternalStorageDirectory();
+    // Generate a unique filename using a timestamp
+    String directoryPath = '$url';
 
-// Create the directory if it doesn't exist
-  Directory(directoryPath).createSync(recursive: true);
+    // Create the directory if it doesn't exist
+    Directory(directoryPath).createSync(recursive: true);
+    return 'File Downloaded';
+  } catch (e) {
+    return '$e';
+  }
 
   // Create an HttpClient instance
-  return 'File Downloaded';
 }
