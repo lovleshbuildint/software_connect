d47965f0b9f2c398e4eb6ec8f4aa362c96cdf524
FlutterFlow <140657486+FlutterFlowEng@users.noreply.github.com>
2024-01-20 04:36:51 +0000

    Updating to latest FlutterFlow output.

--- lib/custom_code/actions/download_file.dart
+++ lib/custom_code/actions/download_file.dart
@@ -28,85 +28,14 @@ Future<String?> downloadFile(String? url, String? extention, String? deviceId,
     String imagesFolderPath = '$directoryPath/$modelName/$bankName';
     String filePath = '$imagesFolderPath/$extention';
 
-// Create the directory if it doesn't exist
-    Directory(imagesFolderPath).createSync(recursive: true);
-
     // Create an HttpClient instance
     final client = http.Client();
 
     // Send the request
     final http.Request request = http.Request('GET', Uri.parse(url!));
     final http.StreamedResponse streamedResponse = await client.send(request);
-
-    // Get the total size of the file
-    int totalBytes = streamedResponse.contentLength ?? 0;
-
-    // Keep track of the downloaded bytes
-    int downloadedBytes = 0;
-
-    // Write the downloaded file to the local storage
-    File file = File(filePath);
-    IOSink sink = file.openWrite();
-    String downloadstatus = "";
-    // Listen for updates during the download
-    FFAppState().update(() {
-      FFAppState().progressBarVisibility = true;
-      FFAppState().softwareDownloadStatus = "Software Downloading...";
-    });
-    streamedResponse.stream.listen(
-      (List<int> chunk) {
-        // Write the chunk to the file
-        sink.add(chunk);
-        // Update the downloaded bytes
-        downloadedBytes += chunk.length;
-        // Calculate and print the percentage
-        double percentage = (downloadedBytes / totalBytes);
-        FFAppState().update(() {
-          FFAppState().percentage = percentage;
-        });
-        // print('Downloading: ${percentage.toStringAsFixed(2)}%');
-      },
-      onDone: () {
-        // Close the file when the download is complete
-        sink.close();
-        client.close(); // Close the HttpClient
-        // Show a message to the user that the file has been downloaded
-        print('File downloaded successfully!');
-        downloadstatus = 'File downloaded successfully!';
-        FFAppState().update(() {
-          FFAppState().softwareDownloadStatus = "Software Downloaded";
-        });
-      },
-      onError: (error) {
-        // Handle errors during the download
-        print('Error downloading file: $error');
-        sink.close();
-        client.close(); // Close the HttpClient
-        file.delete(); // Delete the file in case of an error
-        FFAppState().update(() {
-          FFAppState().softwareDownloadStatus = '$error';
-        });
-      },
-      cancelOnError: true,
-    );
-    print(downloadstatus);
-    return downloadstatus;
-  } catch (e) {
-    try {
-      // Generate a unique filename using a timestamp
-      String directoryPath = '/mnt/media_rw/$deviceId';
-      String imagesFolderPath = '$directoryPath/$modelName/$bankName';
-      String filePath = '$imagesFolderPath/$extention';
-
-// Create the directory if it doesn't exist
+    if (streamedResponse.statusCode == 200) {
       Directory(imagesFolderPath).createSync(recursive: true);
-
-      // Create an HttpClient instance
-      final client = http.Client();
-
-      // Send the request
-      final http.Request request = http.Request('GET', Uri.parse(url!));
-      final http.StreamedResponse streamedResponse = await client.send(request);
 
       // Get the total size of the file
       int totalBytes = streamedResponse.contentLength ?? 0;
@@ -118,11 +47,12 @@ Future<String?> downloadFile(String? url, String? extention, String? deviceId,
       File file = File(filePath);
       IOSink sink = file.openWrite();
       String downloadstatus = "";
-      FFAppState().update(() {
-        FFAppState().softwareDownloadStatus = "Software Downloading...";
-        FFAppState().progressBarVisibility = true;
-      });
       // Listen for updates during the download
+      FFAppState().update(() {
+        FFAppState().progressBarVisibility = true;
+        FFAppState().softwareDownloadStatus = "Software Downloading...";
+      });
+
       streamedResponse.stream.listen(
         (List<int> chunk) {
           // Write the chunk to the file
@@ -141,17 +71,11 @@ Future<String?> downloadFile(String? url, String? extention, String? deviceId,
           sink.close();
           client.close(); // Close the HttpClient
           // Show a message to the user that the file has been downloaded
+          print('File downloaded successfully!');
           downloadstatus = 'File downloaded successfully!';
-          if (downloadedBytes == totalBytes) {
-            FFAppState().update(() {
-              FFAppState().softwareDownloadStatus = "Software Downloaded";
-            });
-          } else {
-            FFAppState().update(() {
-              FFAppState().softwareDownloadStatus =
-                  "File not Downloaded Properly!";
-            });
-          }
+          FFAppState().update(() {
+            FFAppState().softwareDownloadStatus = "Software Downloaded";
+          });
         },
         onError: (error) {
           // Handle errors during the download
@@ -167,6 +91,94 @@ Future<String?> downloadFile(String? url, String? extention, String? deviceId,
       );
       print(downloadstatus);
       return downloadstatus;
+    } else {
+      FFAppState().update(() {
+        FFAppState().softwareDownloadStatus = "File Can't be downloaded";
+      });
+      return 'File Can\'t be downloaded';
+    }
+  } catch (e) {
+    try {
+      // Generate a unique filename using a timestamp
+      String directoryPath = '/mnt/media_rw/$deviceId';
+      String imagesFolderPath = '$directoryPath/$modelName/$bankName';
+      String filePath = '$imagesFolderPath/$extention';
+
+      // Create an HttpClient instance
+      final client = http.Client();
+
+      // Send the request
+      final http.Request request = http.Request('GET', Uri.parse(url!));
+      final http.StreamedResponse streamedResponse = await client.send(request);
+      if (streamedResponse.statusCode == 200) {
+        // Create the directory if it doesn't exist
+        Directory(imagesFolderPath).createSync(recursive: true);
+
+        // Get the total size of the file
+        int totalBytes = streamedResponse.contentLength ?? 0;
+
+        // Keep track of the downloaded bytes
+        int downloadedBytes = 0;
+
+        // Write the downloaded file to the local storage
+        File file = File(filePath);
+        IOSink sink = file.openWrite();
+        String downloadstatus = "";
+        FFAppState().update(() {
+          FFAppState().softwareDownloadStatus = "Software Downloading...";
+          FFAppState().progressBarVisibility = true;
+        });
+        // Listen for updates during the download
+        streamedResponse.stream.listen(
+          (List<int> chunk) {
+            // Write the chunk to the file
+            sink.add(chunk);
+            // Update the downloaded bytes
+            downloadedBytes += chunk.length;
+            // Calculate and print the percentage
+            double percentage = (downloadedBytes / totalBytes);
+            FFAppState().update(() {
+              FFAppState().percentage = percentage;
+            });
+            // print('Downloading: ${percentage.toStringAsFixed(2)}%');
+          },
+          onDone: () {
+            // Close the file when the download is complete
+            sink.close();
+            client.close(); // Close the HttpClient
+            // Show a message to the user that the file has been downloaded
+            downloadstatus = 'File downloaded successfully!';
+            if (downloadedBytes == totalBytes) {
+              FFAppState().update(() {
+                FFAppState().softwareDownloadStatus = "Software Downloaded";
+              });
+            } else {
+              FFAppState().update(() {
+                FFAppState().softwareDownloadStatus =
+                    "File not Downloaded Properly!";
+              });
+            }
+          },
+          onError: (error) {
+            // Handle errors during the download
+            print('Error downloading file: $error');
+            sink.close();
+            client.close(); // Close the HttpClient
+            file.delete(); // Delete the file in case of an error
+            FFAppState().update(() {
+              FFAppState().softwareDownloadStatus = '$error';
+            });
+          },
+          cancelOnError: true,
+        );
+        print(downloadstatus);
+        return downloadstatus;
+      } else {
+        FFAppState().update(() {
+          FFAppState().softwareDownloadStatus = "File Can't be downloaded";
+        });
+        return 'File Can\'t be downloaded';
+      }
     } catch (e) {
       return '$e';
     }
